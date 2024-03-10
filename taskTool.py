c035c687cfa06163bf3f5e2cf065a7f1fee7dd0b
EmiePul <mapulcherie@student.udm.ac.mu>
2024-03-10 22:02:20 +0400

    fonction pour marquer la fin d'une tache

--- taskTool.py
+++ taskTool.py
@@ -8,3 +8,10 @@ def suprimerTache(tache) :
 		del task[tache]
 	else:
 		print ("cette tache n'existe pas")
+
+def marquerFin(tache) :
+	if tache in task :
+		task[tache] = task[tache] + "finish"
+	else :
+		print ("cette tache n'existe pas")
+
