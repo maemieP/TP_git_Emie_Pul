b12c8848388c10e325fa15f50ed397dab2b1f642
EmiePul <mapulcherie@student.udm.ac.mu>
2024-03-10 22:00:04 +0400

    fonction pour suprimer une tache

--- taskTool.py
+++ taskTool.py
@@ -1,3 +1,10 @@
 task = {}
 def ajouterTache(tache, description) :
 	task[tache] = description 
+
+
+def suprimerTache(tache) :
+	if tache in task :
+		del task[tache]
+	else:
+		print ("cette tache n'existe pas")
